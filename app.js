0e2f1e33683dfb1c2d939ab48daaadbb7cb7a342
audunu <audunu@gmail.com>
2022-05-06 10:43:05 +0200

    added ability to copy color code on click of color

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const inputElement = document.querySelector('input')
 const selectElement = document.querySelector('select')
 const mainContainer = document.querySelector('.main')
 const footerContainer = document.querySelector('.footer')
+const popup = document.querySelector('.popup')
 let colorArray = []
 
 document.querySelector('button').addEventListener('click', () => {
@@ -16,6 +17,7 @@ document.querySelector('button').addEventListener('click', () => {
                 colorArray.push(data.colors[i].hex.value)
             }
             render()
+            
         })
 
 
@@ -24,15 +26,41 @@ document.querySelector('button').addEventListener('click', () => {
 
 
 function render() {
-    mainContainer.innerHTML = colorArray.map(color => (
-        `<div style="background-color: ${color}" class="bar"></div>`
+    
+
+
+    mainContainer.innerHTML = colorArray.map((color, index) => (
+        `<div style="background-color: ${color}" class="bar bar${index}"></div>`
     )).join('')
 
     footerContainer.innerHTML = colorArray.map(color => (`
     <div class="foot">${color}</div>`
     )).join('')
+
+    for (let i = 0; i < 5; i++) {
+        document.querySelector(`.bar${i}`).addEventListener('click', copyToClipboard)
+    }
+}
+
+function copyToClipboard(e) {
+    const bar = e.target
+    const bgColor = window.getComputedStyle(bar).backgroundColor
+    const rgb2hex = (rgb) => `#${rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/).slice(1).map(n => parseInt(n, 10).toString(16).padStart(2, '0')).join('')}`
+    const bgColorhex = rgb2hex(bgColor)
+    const text = document.createElement('textarea')
+    document.body.appendChild(text)
+    text.value = bgColorhex
+    text.select()
+    document.execCommand('copy')
+    document.body.removeChild(text)
+    renderPopup(e)
 }
 
 
-
-
+function renderPopup(e) {
+    popup.style.display="block"
+    popup.style.left=`${e.pageX}px`
+    popup.style.top=`${e.pageY}px`
+    console.log(popup, e.pageX)
+    setTimeout(() => popup.style.display="none", 3000)
+}
